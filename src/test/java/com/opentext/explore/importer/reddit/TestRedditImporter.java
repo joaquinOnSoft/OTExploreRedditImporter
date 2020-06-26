66636a5700d6038308c9db5989bf8a1e36a89a03
JoaquinOnSoft <joaquingp@gmail.com>
2020-06-26 10:30:13 +0000

    Test improved

--- src/test/java/com/opentext/explore/importer/reddit/TestRedditImporter.java
+++ src/test/java/com/opentext/explore/importer/reddit/TestRedditImporter.java
@@ -3,14 +3,20 @@ package com.opentext.explore.importer.reddit;
 import org.junit.Test;
 
 import junit.framework.TestCase;
+import net.dean.jraw.models.Listing;
+import net.dean.jraw.models.Submission;
 
 public class TestRedditImporter  extends TestCase {
 
 	@Test
 	public void testStart() {
 		RedditImporter redditImp = new RedditImporter("http://localhost:8983");
-		redditImp.start("CanadaPost", "Reddit");
+		Listing<Submission> firstPage = redditImp.readSubreddit("CanadaPost");
 		
-		//TODO add asserts
+		assertNotNull(firstPage);
+		assertEquals(100, firstPage.size());
+				
+		boolean result = redditImp.solrBatchUpdate("Reddit Canada Post", firstPage);
+		assertTrue(result);
 	}
 }
